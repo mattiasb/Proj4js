d540f6dd2a5e1dc33380c581a89508bd3e82c953
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2012-11-07 00:36:37 +0100

    Add noConflict() support

--- Jakefile.js
+++ Jakefile.js
@@ -1,14 +1,30 @@
 var $ = require('fquery');
 
 // TODO: Factor out into build settings file
-var files = ['Proj4js.js'
-			 , 'Class.js'
-			 , 'Common.js'
-			 , 'Point.js'
-			 , 'Datum.js'
-			 , 'Proj.js'
-			 , 'projCode/*.js'
-			];
+var defaultFiles = [  'Proj4js.js'
+					, 'Class.js'
+					, 'Common.js'
+					, 'Point.js'
+					, 'Datum.js'
+					, 'Proj.js'
+					, 'projCode/*.js'
+				   ];
+
+// Helpers
+
+var license = $('LICENSE')
+		.wrap('/*\n','\n*/')
+		.content();
+
+function compile(files){
+	var all = Array.isArray(files)
+			? defaultFiles.concat(files)
+			: defaultFiles;
+	return $('lib:' + all.join(", "))
+		.concat('\n\n');
+}
+
+////// Tasks
 
 task('default', ['build:all']);
 
@@ -28,19 +44,27 @@ namespace('build', function(){
 	desc('Build a classic browser-global version for use without a '
 		 + 'module loader.');
 	task('browser', ['jshint'], function(){
-		compile()
+		compile(["NoConflict.js"])
+			.wrap(license + "\n\n(function(){\n\n",
+				 "\n\n})();")
 			.write($.OVERWRITE, 'dist/proj4js.js')
 			.uglifyjs()
-			.wrap(license())
+			.wrap(license)
 			.write($.OVERWRITE, 'dist/proj4js.min.js');
 	});
 
-	desc('Build a commonjs module for use in for example node.js');
+	desc('Build a commonjs module for use in the browser');
 	task('commonjs', ['jshint'], function(){
 		compile()
-			.wrap('', 'module.exports = Proj4js;')
+			.wrap(license, '\n\n' + 'module.exports = Proj4js;')
 			.write($.OVERWRITE, 'dist/proj4js.commonjs.js');
+	});
 
+	desc('Build a commonjs module for use in Node.js');
+	task('node', ['jshint'], function(){
+		compile()
+			.wrap(license, '\n\n' + 'module.exports = Proj4js;')
+			.write($.OVERWRITE, 'dist/proj4js.node.js');
 	});
 
 	desc('Build an AMD module to be loaded by for example require.js).');
@@ -48,18 +72,7 @@ namespace('build', function(){
 		compile()
 			.wrap('define(function () {'
 				  ,'return Proj4js; });')
+			.wrap(license)
 			.write($.OVERWRITE, 'dist/proj4js.amd.js');
 	});
 });
-
-// Helpers
-
-function compile(){
-	return $('lib:' + files.join(", ")).concat(';');
-}
-
-function license(){
-	return $('LICENSE')
-		.wrap('/*\n','\n*/')
-		.content();
-}
--- demo/leaflet/script.js
+++ demo/leaflet/script.js
@@ -1,8 +1,10 @@
 var res = [8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0.5];
+
 var proj = new Proj4js.Proj('EPSG:2400'
-							, '+lon_0=15.808277777799999 +lat_0=0.0 +k=1.0 +x_0=1500000.0 '
-							+ '+y_0=0.0 +proj=tmerc +ellps=bessel +units=m '
-							+ '+towgs84=414.1,41.3,603.1,-0.855,2.141,-7.023,0 +no_defs');
+					  , '+lon_0=15.808277777799999 +lat_0=0.0 +k=1.0 +x_0=1500000.0 '
+					  + '+y_0=0.0 +proj=tmerc +ellps=bessel +units=m '
+					  + '+towgs84=414.1,41.3,603.1,-0.855,2.141,-7.023,0 +no_defs');
+
 var  crs = L.CRS.proj4js(proj);
 crs.scale = function(zoom) {
     return 1 / res[zoom];
